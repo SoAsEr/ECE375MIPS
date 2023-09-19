e7b55fbe088e72d0543de726319a16e55bdeb4dd
Priya K. Naphade <pnaphade@davisson.princeton.edu>
2023-09-19 12:29:55 -0400

    sub, subu testing

---
+++ test/sub/sub.asm
@@ -0,0 +1,5 @@
+addi $t0, $t0, 0x3      # t0 = 3
+addi $t1, $t1, 0x2		# t1 = 2
+sub $t2, $t0, $t1   	# t2 = 3 - 2 = 1
+sub $t4, $t3, $t2		# underflow, should exit with code 127: t3 = 0 - 1
+.word 0xfeedfeed
